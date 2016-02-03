d86640951d5f705837108ccfcf7549c8a7ca18cb
Petteri Pesonen <teemu.p.pesonen@aalto.fi>
2016-02-03 20:32:55 +0200

    Update main.js

--- main.js
+++ main.js
@@ -236,6 +236,24 @@ function init() {
   	}
   });
   
+  var info = L.control();
+
+info.onAdd = function (map) {
+    this._div = L.DomUtil.create('div', 'info'); // create a div with a class "info"
+    this.update();
+    return this._div;
+};
+
+// method that we will use to update the control based on feature properties passed
+info.update = function (props) {
+    this._div.innerHTML = props.nimi;
+};
+
+info.addTo(map);
+  
+  
+  
+  
   //Tasojen funktioita: kohteeseen zoomaus ja kohteen korostus
   function zoomToFeature(e) {
     map.fitBounds(e.target.getBounds());
