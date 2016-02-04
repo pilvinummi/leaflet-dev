2aebbd1fb6da8663d290e860db939f4e60ee4ef3
Petteri Pesonen <teemu.p.pesonen@aalto.fi>
2016-02-04 17:34:52 +0200

    Update main.js

--- main.js
+++ main.js
@@ -492,5 +492,27 @@ function init() {
     }
   });
 
+
+  var legend = L.control({position: 'bottomleft'});
+
+  legend.onAdd = function (map) {
+
+    var div = L.DomUtil.create('div', 'info legend'),
+        grades = [0, 1000, 2000, 4000, 6000, 8000, 10000, 12000],
+        labels = [];
+        
+    // loop through our density intervals and generate a label with a colored square for each interval
+    for (var i = 0; i < grades.length; i++) {
+        div.innerHTML +=
+            '<i style="background:' + getColor(grades[i] + 1) + '"></i> ' +
+            grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
+    }
+
+    return div;
+  };
+
+  legend.addTo(map);
+
+
 	
 }
