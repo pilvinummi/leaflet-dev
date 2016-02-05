0f6e38de9f166b387d1468c50eea66ff658be3b4
Petteri Pesonen <teemu.p.pesonen@aalto.fi>
2016-02-05 15:29:05 +0200

    Update main.js

--- main.js
+++ main.js
@@ -150,19 +150,19 @@ function init() {
     //Jos bufferin sade on asetettu null, niin ei pitaisi pystya luomaan popupia (jos laittaa !=, niin silloin
     //popupia ei saa luotua missaan tilanteessa
     popupOptions = {maxWidth: 200};
-    layer.unbindPopup();
+    var rad = window.radius
     
-    //layer.on('click', function() {
-      if (window.radius == null) {
-        layer.bindPopup("<b>Viheralueen tunnus: </b> " + feature.properties.viheralue_id +
-          "<br><b>Nimi: </b> " + feature.properties.puiston_nimi +
-          "<br><b>Käyttötarkoitus: </b> " + feature.properties.kayttotarkoitus +
-          "<br><b>Käyttötarkoitus id: </b> " + feature.properties.kayttotarkoitus_id +
-          "<br><b>Pinta-ala: </b> " + feature.properties.pinta_ala
-          ,popupOptions);
+    if (rad == null) {
+      layer.bindPopup("<b>Viheralueen tunnus: </b> " + feature.properties.viheralue_id +
+        "<br><b>Nimi: </b> " + feature.properties.puiston_nimi +
+        "<br><b>Käyttötarkoitus: </b> " + feature.properties.kayttotarkoitus +
+        "<br><b>Käyttötarkoitus id: </b> " + feature.properties.kayttotarkoitus_id +
+        "<br><b>Pinta-ala: </b> " + feature.properties.pinta_ala
+        ,popupOptions);
+      } else {
+      	layer.unbindPopup();
       }
-    //});
-    
+
     layer.on({
       mousemove: mousemove,
       mouseout: mouseout, 
