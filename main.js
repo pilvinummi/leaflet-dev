6eb82116e2fb9b7506c8925c2944991154bc561e
Petteri Pesonen <teemu.p.pesonen@aalto.fi>
2016-02-05 14:30:42 +0200

    Update main.js

--- main.js
+++ main.js
@@ -30,8 +30,8 @@ function init() {
   
   var map = L.map('map', {
     //center: new L.LatLng(60.192871, 25.049858), //(60.1708, 24.9375),
+    maxBounds: bounds,
     zoom: 12,
-    //maxBounds: bounds,
     maxZoom: 18,
     minZoom: 11
   });
