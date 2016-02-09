58112d4c61c33709ad16229a09eaee9844b7fb22
pilvinummi <pilkku@gmail.com>
2016-02-09 09:48:55 +0200

    Update main.js

--- main.js
+++ main.js
@@ -98,14 +98,14 @@ function init() {
       
             if ( kaytto.indexOf("semakaavoitettu") > -1) fillColor = "#3333cc";
             else if ( kaytto == "Yleiskaavan viheralue" ) fillColor = "#663399";
-            else if ( kaytto == "Ulkoilumetsä" ) fillColor = "#0099cc";
+            else if ( kaytto == "Ulkoilumetsä" ) fillColor = "#339966";
             else if ( kaytto == "Kartano- ja huvila-alue" ) fillColor = "#990099";
             else if ( kaytto == "Kesämaja-alue" || kaytto == "Siirtolapuutarha" || kaytto == "Viljelypalsta" || kaytto == "Viljelypalsta-alue") fillColor = "#006699";
             else if ( kaytto == "Koira-aitaus" ) fillColor = "#00cc99";
             else if ( kaytto == "Leikkipaikka" || kaytto == "Leikkipuisto" ) fillColor = "#339999";
-            else if ( kaytto == "Luonnonsuojelualue" ) fillColor = "#00cccc";
+            else if ( kaytto == "Luonnonsuojelualue" ) fillColor = "#00cc99";
             else if ( kaytto.indexOf("luonnonsuojelualue") > -1 ) fillColor = "#6666cc";
-            else if ( kaytto == "Uimaranta-alue" || kaytto == "Venesatama / Venevalkama" ) fillColor = "#cc0099";
+            else if ( kaytto == "Uimaranta-alue" || kaytto == "Venesatama / Venevalkama" ) fillColor = "#00cccc";
             else if ( kaytto.indexOf("Haudat") > -1 ) fillColor = "#006666";
             else if ( kaytto == "Muut viheralueet" ) fillColor = "#000066";
             else fillColor = "#999";  // no data
